1b37b637d893b3e999376f09284a0ac10ce8a524
yl642 <yl642@duke.edu>
2020-01-24 12:12:32 -0500

    Add basic outline for interface

---
+++ interface.py
@@ -0,0 +1,10 @@
+def interface():
+    print("Cholesterol Analysis")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+
+if __name__ == '__main__':
+        interface()
