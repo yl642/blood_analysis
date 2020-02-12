4ee4b2111c0de47ddb7ac8fe95ff2bee783e15a5
yl642 <yl642@duke.edu>
2020-02-12 15:28:59 -0500

    A Bad Commit

--- interface.py
+++ interface.py
@@ -6,6 +6,7 @@ def HDL_analysis(HDL_level):
     else :
         return 'Low'
 
+
 def LDL_analysis(LDL_level):
     if LDL_level <= 130:
         return 'Nromal'
@@ -16,6 +17,7 @@ def LDL_analysis(LDL_level):
     elif 190 <= LDL_level:
         return 'Very high'
 
+
 def TOT_analysis(TOT_level):
     if TOT_level < 200:
         return 'Nromal'
@@ -24,27 +26,62 @@ def TOT_analysis(TOT_level):
     elif 240 <= TOT_level:
         return 'High'
 
+
 def cholesterol_analysis():
     print('Cholesterol Check')
     HDLinput = input('Enter test result: ')
     test_info = HDLinput.split('=')
     if test_info[0].strip() == 'HDL':
         print(HDL_analysis(int(test_info[1])))
+        save_result('HDL', HDL_analysis(int(test_info[1])), test_info[1])
     elif test_info[0].strip() == 'LDL':
         print(LDL_analysis(int(test_info[1])))
+        save_result('LDL', LDL_analysis(int(test_info[1])), test_info[1])
     elif test_info[0].strip() == 'TOT':
         print(TOT_analysis(int(test_info[1])))
+        save_result('TOT', TOT_analysis(int(test_info[1])), test_info[1])
+
+
+def save_info():
+    import json
+    name = input('first name and last name:')
+    age = input('age:')
+    info_dict = {'full name': name, 'age': age}
+    file = open('info.json', 'w')
+    json.dump(info_dict, file)
+    file.close()
+    return info_dict
+
+
+def save_result(x, y, z):
+    import json
+    r = y + ' (' + z.strip() + ')'
+    file = open('info.json', 'w+') % 'r'
+    result_dict = json.load(file)
+    #file.close()
+    result_dict[x] = r
+    #file = open('info.json', 'w')
+    json.dump(result_dict, file)
+    file.close()
+    return
+
 
 def interface():
     while 1:
         print("Cholesterol Analysis")
         print("Options:")
         print("1 - Cholesterol Check")
+        print("2 - Record Patient Info")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice == '9':
             return
         elif choice == '1':
             cholesterol_analysis()
+        elif choice == '2':
+            a = save_info()
+            print(a)
+
+
 if __name__ == '__main__':
         interface()
